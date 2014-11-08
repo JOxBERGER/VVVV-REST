f791e6a75400afd528c73bc2e358f70014b1c645
Jochen Leinberger <leinberger@explorative-environments.net>
2014-11-08 20:17:03 +0100

    rest client up and running

--- plugins/0NetworkHTTP-REST/C0NetworkHTTP_RESTNode.cs
+++ plugins/0NetworkHTTP-REST/C0NetworkHTTP_RESTNode.cs
@@ -1,5 +1,6 @@
 #region usings
 using System;
+using System.IO;
 using System.ComponentModel.Composition;
 
 using VVVV.PluginInterfaces.V1;
@@ -24,15 +25,12 @@ namespace VVVV.Nodes
 	{
 		#region enums
 		public enum HttpMethod { GET, POST, PUT, DELETE }
-		//public enum RequestFormat { JSON, XML }
+		public enum RequestFormat { JSON, XML }
 		#endregion enums
 		
 		#region fields & pins
 		[Input("BaseURL", StringType = StringType.URL, DefaultString = "http://localhost")]
 		public ISpread<string> FInputBaseURL;
-		
-		[Input("Ressource", DefaultString = null)]
-		public ISpread<string> FInputResourcePath;
 		
 		[Input("HttpMethod", DefaultEnumEntry = "GET")]
 		public ISpread<HttpMethod> FInputHttpMethod;
@@ -40,8 +38,11 @@ namespace VVVV.Nodes
 		[Input("MimeType", DefaultString ="")]
 		public ISpread<string> FInputMimeType;
 		
-		//[Input("RequestFormat", DefaultEnumEntry = "JSON")]
-		//public ISpread<RequestFormat> FInputRequestFormat;
+		[Input("RequestFormat", DefaultEnumEntry = "JSON")]
+		public ISpread<RequestFormat> FInputRequestFormat;
+		
+		[Input("Message Body", DefaultString = null)]
+		public ISpread<string> FInputMessageBody;
 		
 		[Input("Use Basic Authentication", IsBang = false, DefaultValue = 0, IsSingle = false)]
 		IDiffSpread<bool> FInputUseBasicAuthentication;
@@ -52,14 +53,17 @@ namespace VVVV.Nodes
 		[Input("Password", DefaultString = null)]
 		public ISpread<string> FInputPassword;
 		
-		[Input("Content", DefaultString = null)]
-		public ISpread<string> FInputContent;
-		
 		[Input("Execute", IsBang = true, DefaultValue = 0, IsSingle = false)]
 		IDiffSpread<bool> FInputExecute;
 
 		[Output("Output")]
 		public ISpread<string> FOutputResponse;
+		
+		[Output("Header")]
+        public ISpread<string>  FOutputHeader;
+		
+		[Output("Success", DefaultValue = 0)]
+		public ISpread<bool> FOutputSuccess;
 		
 		[Output("Status")]
 		public ISpread<string> FOutputStatus;
@@ -73,14 +77,26 @@ namespace VVVV.Nodes
 		{
 			
 			FOutputResponse.SliceCount = SpreadMax;
+			FOutputHeader.SliceCount = SpreadMax;
+			FOutputSuccess.SliceCount = SpreadMax;
 			FOutputStatus.SliceCount = SpreadMax;
+
 			//// start doing stuff foreach spread item
 			for (int i = 0; i < SpreadMax; i++)
 			{
+				
 				try{
-					var client = new RestClient();
+					
+					
 					if (FInputExecute[i])
 					{
+						// Clear Fields
+						FOutputResponse[i] = "";
+						FOutputHeader[i] = "";
+						FOutputSuccess[i] = false;
+						FOutputStatus[i] = "";
+						
+						var client = new RestClient();
 						client.BaseUrl = new Uri(FInputBaseURL[i]);
 						
 						if(FInputUseBasicAuthentication[i])
@@ -110,7 +126,9 @@ namespace VVVV.Nodes
 						}
 						
 						
-						/*
+						
+						
+						
 						//  defines the Request serialisation format
 						switch(FInputRequestFormat[i].ToString())
 						{
@@ -121,15 +139,15 @@ namespace VVVV.Nodes
 						case "XML":
 							request.RequestFormat = DataFormat.Xml;
 							break;
-						}*/
+						}
 						
 
-						request.Resource = FInputResourcePath[i];
+						//request.Resource = FInputResourcePath[i];
 						//request.AddHeader("Content-type", "application/json; charset=utf-8");
 						//request.AddBody(FInputContent[i]);
 						
 						
-						request.AddParameter(FInputMimeType[i], FInputContent[i], ParameterType.RequestBody);
+						request.AddParameter(FInputMimeType[i], FInputMessageBody[i], ParameterType.RequestBody);
 						
 						
 						
@@ -141,22 +159,59 @@ namespace VVVV.Nodes
 						
 						
 						IRestResponse response = client.Execute(request);
+						
 						FOutputResponse[i] = response.Content;
-						FOutputStatus[i] = "ResponseStatus:       "+response.ResponseStatus.ToString()+"\n";
-						FOutputStatus[i] += "StatusDescription:   "+response.StatusDescription +"\n";
-						FOutputStatus[i] += "Request:             "+response.Request +"\n";
-						FOutputStatus[i] += "ResponseUri:         "+response.ResponseUri +"\n";
-						FOutputStatus[i] += "ErrorException:      "+response.ErrorException +"\n";
-						FOutputStatus[i] += "ErrorMessage:        "+response.ErrorMessage +"\n";
+						
+						if(response.StatusCode.ToString() == "OK"|| response.StatusCode.ToString() == "Created")
+						{
+						FOutputSuccess[i] = true;
+						}
+						else
+						{
+						FOutputSuccess[i] = false;							
+						}
+						
+						
+						
+						// get Complete Header
+						int FHeaderItems = response.Headers.Count;
+						if(FHeaderItems > 0)
+						{
+							FOutputHeader[i] = "";
+							for(int j=0; j < FHeaderItems; j++)
+							{
+								FOutputHeader[i] += j.ToString()+" "+response.Headers[j]+"\n";
+							}
+						}
+						
+						
+						FOutputStatus[i] += "Time:		"+getTimestamp()+"\n";
+						FOutputStatus[i] += "ResponseStatus:	"+response.ResponseStatus+"\n";
+						FOutputStatus[i] += "StatusDescription:	"+response.StatusDescription +"\n";
+						FOutputStatus[i] += "StatusCode:	"+response.StatusCode.ToString() +"\n";
+						FOutputStatus[i] += "Request:		"+response.Request.RequestFormat +"\n";
+						FOutputStatus[i] += "ResponseUri:	"+response.ResponseUri +"\n";
+						//FOutputStatus[i] += "Headers:	"+response.Headers[0];//Count.ToString() +"\n";
+						
+						//FOutputStatus[i] += "ErrorException:	"+response.ErrorException.Message.ToString() +"\n";
+						//FOutputStatus[i] += "ErrorMessage:	"+response.ErrorMessage +"\n";
 					}
 					
 				}
 				catch (Exception e)
 				{
 					FLogger.Log(e);
+					FOutputStatus[i] = e.Message.ToString();
+					FOutputSuccess[i] = false;	
 				}
 				
 			}	
+		}
+		
+		private string getTimestamp()
+		{
+			string timeStamp = DateTime.Now.ToString();
+			return timeStamp;
 		}
 
 		//FLogger.Log(LogType.Debug, "Logging to Renderer (TTY)");
